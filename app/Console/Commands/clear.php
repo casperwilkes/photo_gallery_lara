7fe8a617d40a1f6eee138b1406fffdfec4bed828
Casper Wilkes <casper@casperwilkes.net>
2018-06-01 11:33:13 -0400

    Built an Artisan cleaner command for cleaning up all cache/log/img files in system Registered command in the Kernel runner

---
+++ app/Console/Commands/clear.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Log;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+class clear extends Command {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear '
+                           . '{--m|migrate : Refresh migrations '
+                           . '(run php artisan migrate:refresh -h for more)} '
+                           . '{--s|seed : Re-seed the database '
+                           . '(run php artisan db:seed -h for more)}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clears all cache files and logs from application';
+
+    /**
+     * Progress bar
+     * @var ProgressBar
+     */
+    //private $bar;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        // Initial procs for bar //
+        // One for every task //
+        $procs = '6';
+
+        // Get migrate option //
+        $migration = $this->option('migrate');
+
+        $seed = $this->option('seed');
+
+        // Migrate option called //
+        if ($migration) {
+            // Up the procs //
+            $procs++;
+        }
+
+        // Seed option called //
+        if ($seed) {
+            // Up the procs //
+            $procs++;
+        }
+
+        // Init the progress bar //
+        $bar = $this->output->createProgressBar($procs);
+
+        $this->comment('Start Clearing Files:');
+
+        // Logs //
+        $this->logs($bar);
+
+        // Compiled //
+        $this->compiled($bar);
+
+        // Cache //
+        $this->cache($bar);
+
+        // Configs //
+        $this->configs($bar);
+
+        // Routes //
+        $this->routes($bar);
+
+        // Images //
+        $this->images($bar);
+
+        // Run migrations //
+        if ($migration) {
+            $this->migrations($bar);
+        }
+
+        // Re-seed db //
+        if ($seed) {
+            $this->seed($bar);
+        }
+
+        $this->comment('Total Progress');
+
+        $bar->finish();
+
+        $this->comment("\nFinished Clearing Files.");
+    }
+
+    /**
+     * Cleans the log files
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function logs(ProgressBar $bar) {
+        $this->line('Logs:');
+
+        // Get all log files //
+        $logs = glob(storage_path('logs/*.log'));
+
+        // Loop through and remove //
+        foreach ($logs as $log) {
+            unlink($log);
+        }
+        $this->info('Log files have been cleared!');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Clears compiled files
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function compiled(ProgressBar $bar) {
+        $this->line('Compiled Services:');
+        $this->call('clear-compiled');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Clears cache files
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function cache(ProgressBar $bar) {
+        $this->line('Cache:');
+        $this->call('cache:clear');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Clears config cache
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function configs(ProgressBar $bar) {
+        $this->line('Config:');
+        $this->call('config:clear');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Clears the route cache
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function routes(ProgressBar $bar) {
+        $this->line('Routes:');
+        $this->call('route:clear');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Deletes image files
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function images(ProgressBar $bar) {
+        // Logs //
+        $this->line('Images:');
+
+        // Img formats to match //
+        $img_mimes = array('gif', 'jpg', 'jpeg', 'png');
+
+        // Path to the img directory //
+        $img_path = public_path('img/main/');
+
+        // What to match //
+        $glob_match = '*.{' . implode(',', $img_mimes) . '}';
+
+        // Get all img files matching img mimes //
+        $main = glob($img_path . $glob_match, GLOB_BRACE);
+
+        // Thumbnails //
+        $thumb = glob($img_path . 'thumb/' . $glob_match, GLOB_BRACE);
+
+        // Loop through and remove //
+        // Main imgs //
+        foreach ($main as $m) {
+            unlink($m);
+        }
+
+        // Thumbnails //
+        foreach ($thumb as $t) {
+            unlink($t);
+        }
+
+        // Avatars //
+
+        $this->info('Image files successfully cleared!');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Refreshes and calls the status on migrations
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function migrations(ProgressBar $bar) {
+        $this->line('Migrations refreshing:');
+        $this->call('migrate:refresh');
+
+        $this->info('Migration refresh complete.');
+
+        $this->line('Migrations status:');
+
+        $this->call('migrate:status');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+
+    /**
+     * Re-seeds the database with mock data
+     * @param ProgressBar $bar
+     * @return bool
+     */
+    private function seed(ProgressBar $bar) {
+        $this->line('Re-seeding database:');
+        $this->call('db:seed');
+
+        $bar->advance();
+
+        $this->info('');
+
+        return true;
+    }
+}
